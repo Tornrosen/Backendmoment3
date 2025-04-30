93ce392aef20bdfa33003b418c3879876c04ba5c
Tornrosen <elec11anna@hotmail.com>
2025-04-30 14:30:42 +0200

    skapa utvecklingsmiljö, skapa anslutning databas

---
+++ server.js
@@ -0,0 +1,56 @@
+/*
+Webbapplikation av Anna Danielsson */
+
+//Lägga till MongoDB, mongoose och Express
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+//Starta upp Express
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+//ansluta till mongoose
+mongoose.connect("mongodb://127.0.0.1:27017/jobs").then(() =>{
+    console.log("Ansluten till MongoDB.")
+}).catch((error)=>{
+    console.log("Fel vid anslutning till databas: " + error)
+})
+
+//skapa schema för dokument
+const JobSchema = new mongoose.Schema({
+    title: {
+        type:String, required:true
+    },
+    workplace: {
+        type:String, required:true
+    },
+    employer: {
+        type:String, required:true
+    },
+    startdate: {
+        type:String, required:true
+    },
+    enddate: {
+        type:String, required:true
+    },
+    description: {
+        type:String, required:true
+    }
+})
+
+//inkludera schema till databas
+const Job = mongoose.model("Job", JobSchema)
+
+//Skapa routes
+app.get("/api", async (req, res) => {
+    res.json({message:"Welcome to my API."})
+});
+
+app.listen(port, ()=> {
+    console.log ("Servern är igång på port: " + port)
+});
+
